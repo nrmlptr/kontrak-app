247f05fc27d378e0e6b6b16a26bdbabc71b856a3
nramalptr <nfzptr@gmail.com>
2024-07-03 14:58:41 +0700

    update percobaan sinkron dg laravel schedule

---
+++ app/Console/Commands/SyncDataSOP.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\KontrakController;
+
+class SyncDataSOP extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sync-data-sop';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sinkronisasi data SOP Terbaru dari SCM';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //KODE
+        $kontrakController = new KontrakController();
+        $kontrakController->syncron();
+
+        $this->info('Data SOP Berhasil di Sinkronisasi!');
+    }
+}
